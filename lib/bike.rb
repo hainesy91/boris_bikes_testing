7f17dbcdcbb7856c005898a05ccdd36fffdb097b
Christopher Haines <hainesy91@gmail.com>
2015-05-21 16:18:42 +0100

    created a second text that passed

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,7 +1,16 @@
 class Bike
 
+    def initialize
+      @broken = false
+
+    end
+
   def broken?
-    false
+    @broken
+  end
+
+  def break
+    @broken = true
   end
 
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -7,7 +7,12 @@ describe Bike do
   it 'should not be broken when created' do
     bike = Bike.new #Instantiate a new instance of the Bike class
     expect(bike.broken?).to be false
-    
+  end
+
+  it 'should be able to break' do
+    bike = Bike.new
+    bike.break
+    expect(bike.broken?).to be true
   end
 
 end
