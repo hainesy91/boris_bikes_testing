d72570818881d9a8ec1309ce012ac13496665ca9
Christopher Haines <hainesy91@gmail.com>
2015-05-21 19:13:44 +0100

    solved the tests for the docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,8 @@
 class DockingStation
+  DEFAULT_CAPACITY = 10 
 
-  def initialize
+  def initialize(options = {})
+    @capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
     @bikes = []
   end
 
@@ -8,12 +10,21 @@ class DockingStation
     @bikes.count
   end
 
-  def dock (bike)
+  def dock bike
     @bikes << bike
   end
 
   def release(bike)
     @bikes.delete(bike)
+  end
+
+  def full?
+    bike_count == @capacity
+  end
+  
+  def dock(bike)
+  raise "Station is full" if full?
+    @bikes << bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,20 +1,33 @@
 require_relative '../lib/docking_station'
 
-describe DockingStation do 
+describe DockingStation do
 
-  let(:bike)  { Bike.new }
-  let(:station) { DockingStation.new }
-
-  it 'should be able to accept a bike' do
+  let(:station) { DockingStation.new(:capacity => 20) } 
+  let(:bike) { Bike.new }
+  
+  it 'should be able to accept a bike' do 
+    bike = Bike.new
     expect(station.bike_count).to eq 0
     station.dock(bike)
     expect(station.bike_count).to eq 1
   end
 
+
   it "should release a bike" do
-    station.dock(bike)
-    station.release(bike)
-    expect(station.bike_count).to eq 0
+     station.dock(bike)
+     station.release(bike)
+     expect(station.bike_count).to eq 0
+  end
+
+  it "should know when it's full" do
+    expect(station.full?).not_to be true
+    20.times { station.dock(Bike.new) }
+    expect(station.full?).to be true
+  end
+
+  it "should not accept a bike if it's full" do
+    20.times { station.dock(Bike.new) }
+    expect { station.dock(bike) }.to raise_error RuntimeError
   end
 
 end
